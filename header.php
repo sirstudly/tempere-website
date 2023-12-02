f1c7c5c3a4193804d91c500463ee27a6b25523ec
sirstudly <ron1404@techie.com>
2023-12-02 19:30:55 +0000

    Added facebook verification meta tag

--- header.php
+++ header.php
@@ -2,3 +2,4 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
     <title>Tempere Consulting Limited</title>
+    <meta name="facebook-domain-verification" content="rgg2wq0byfgqpwbrxpb47ma01zjtgl" />
